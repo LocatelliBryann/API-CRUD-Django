04cefde07b8c7a9a42b65fa054a11f13642f554b
LocatelliBryann <bryannlocatelli@gmail.com>
2025-05-13 20:25:50 -0300

    update urls.py

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('usuarios/', views.listar_usuarios, name='listar_usuarios'),  # Para GET
-    path('usuarios/criar/', views.criar_usuario, name='criar_usuario'),  # Para POST
-    path('usuarios/atualizar/<int:pk>/', views.atualizar_usuario, name='atualizar_usuario'),  # Para PUT
-    path('usuarios/excluir/<int:pk>/', views.excluir_usuario, name='excluir_usuario'),  # Para DELETE
+    path('usuarios/', views.listar_usuarios, name='listar_usuarios'),
+    path('usuarios/criar/', views.criar_usuario, name='criar_usuario'),
+    path('usuarios/atualizar/<int:pk>/', views.atualizar_usuario, name='atualizar_usuario'),
+    path('usuarios/excluir/<int:pk>/', views.excluir_usuario, name='excluir_usuario'),
 ]
